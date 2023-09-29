d1c30b332208d76040973e664f84bfaf519422b6
Jai <80166407+jairajput@users.noreply.github.com>
2023-09-29 20:58:03 +0530

    Update

--- VerbalEnigma/view/KeyCell.swift
+++ VerbalEnigma/view/KeyCell.swift
@@ -4,26 +4,42 @@
 //
 //  Created by Jai  on 29/09/23.
 //
-
 import UIKit
 
-class KeyCell: UIViewController {
+class KeyCell: UICollectionViewCell {
+    static let identifier = "KeyCell"
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    let label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 18, weight: .medium)
+        return label
+    }()
 
-        // Do any additional setup after loading the view.
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemGray5
+        contentView.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            label.topAnchor.constraint(equalTo: contentView.topAnchor),
+            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+        ])
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError()
     }
-    */
 
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        label.text = nil
+    }
+
+    func configure(with letter: Character) {
+        label.text = String(letter).uppercased()
+    }
 }
